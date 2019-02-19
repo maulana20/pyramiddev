ebd9c37d1ef165914eed36fe160b0bce930a678f
maulana <maulana20@versatech.co.id>
2019-02-19 12:07:43 +0700

    telegram fix
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- model/telegrambot.py
+++ model/telegrambot.py
@@ -4,12 +4,13 @@ import time
 
 class TelegramBot:
 	def __init__(self, url, token):
+		self.request = requests.session()
 		self.url = url + '/bot' + token
 		self.update_id = 0
 	
 	def send(self, method, params):
 		host_url = self.url + '/' + method
-		response = requests.get(host_url, data=params);
+		response = self.request.get(host_url, data=params);
 		res_json = json.loads(response.text)
 		
 		return res_json['result']
@@ -20,7 +21,6 @@ class TelegramBot:
 		
 		if len(last_update):
 			self.update_id = last_update[-1]['update_id']
-			print(self.update_id)
 			last_update = self.send('getUpdates', {'offset': self.update_id})
 			return last_update[0]['message']
 		else:
@@ -32,7 +32,7 @@ class TelegramBot:
 		
 		if len(refresh_update):
 			self.update_id = refresh_update[0]['update_id']
-			print(self.update_id)
+			return refresh_update[0]['message']
 			
 	def message(self, chat_id, text):
 		self.send('sendMessage', {'chat_id': chat_id, 'text': text})
@@ -40,21 +40,22 @@ class TelegramBot:
 		return
 		
 	def run(self):
-		last_update = self.last_update()
+		data = []
+		data = self.last_update()
 		update_id = self.update_id + 1
 		
 		while True:
 			if self.update_id > 0:
-				self.refresh_update(update_id)
+				data = self.refresh_update(update_id)
 			
 			if update_id == self.update_id:
-				chat_id = last_update['chat']['id']
-				text = last_update['text']
+				chat_id = data['chat']['id']
+				text = data['text']
 				self.message(chat_id, text)
 				
 				update_id = update_id + 1
 				
-				print('listeningx ..')
+				print(data['chat']['username'] + '(' + str(chat_id) + ')' + ' => ' + text)
 			else:
 				print('listening ..')
 			time.sleep(1)
