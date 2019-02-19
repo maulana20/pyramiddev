9d92649aa944f42e06b8e2d9f24bd81e77df7e65
maulana <maulana20@versatech.co.id>
2019-02-19 10:50:24 +0700

    update run looping
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- model/telegrambot.py
+++ model/telegrambot.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import time
 
 class TelegramBot:
 	def __init__(self, url, token):
 		self.url = url + '/bot' + token
+		self.update_id = 0
 	
 	def send(self, method, params):
 		host_url = self.url + '/' + method
@@ -17,29 +19,48 @@ class TelegramBot:
 		last_update = self.send('getUpdates', {})
 		
 		if len(last_update):
-			last_update = self.send('getUpdates', {'offset': last_update[-1]['update_id']})
+			self.update_id = last_update[-1]['update_id']
+			print(self.update_id)
+			last_update = self.send('getUpdates', {'offset': self.update_id})
+			return last_update[0]['message']
+		else:
+			return last_update
 		
-		return last_update[0]['message']
+	def refresh_update(self, update_id):
+		refresh_update = []
+		refresh_update = self.send('getUpdates', {'offset': update_id})
 		
+		if len(refresh_update):
+			self.update_id = refresh_update[0]['update_id']
+			print(self.update_id)
+			
 	def message(self, chat_id, text):
 		self.send('sendMessage', {'chat_id': chat_id, 'text': text})
 		
 		return
 		
 	def run(self):
-		last_update = []
 		last_update = self.last_update()
+		update_id = self.update_id + 1
 		
-		if len(last_update):
-			chat_id = last_update['chat']['id']
-			text = last_update['text']
-			self.message(chat_id, text)
-		
-		return 'listening ..'
+		while True:
+			if self.update_id > 0:
+				self.refresh_update(update_id)
+			
+			if update_id == self.update_id:
+				chat_id = last_update['chat']['id']
+				text = last_update['text']
+				self.message(chat_id, text)
+				
+				update_id = update_id + 1
+				
+				print('listeningx ..')
+			else:
+				print('listening ..')
+			time.sleep(1)
 
 host = 'https://api.telegram.org'
 token = '582054434:AAEVQ8Cdihvhw1jHv5cO-AJfF6xzLkbhugE'
 
 telegram = TelegramBot(host, token)
-last_update = telegram.run()
-print(last_update)
+telegram.run()
